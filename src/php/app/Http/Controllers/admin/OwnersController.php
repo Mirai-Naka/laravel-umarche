a3f67e9aaa34496ae0fca814176c3def5a773eb1
mirai <nakajimami11@gmail.com>
2023-03-31 16:41:56 +0900

    削除機能の実装

--- src/php/app/Http/Controllers/admin/OwnersController.php
+++ src/php/app/Http/Controllers/admin/OwnersController.php
@@ -73,7 +73,10 @@ class OwnersController extends Controller
         ]);
 
 
-        return redirect()->route('admin.owners.index')->with('message', 'オーナー登録を実施しました。');
+        return redirect()->route('admin.owners.index')->with([
+            'message' => 'オーナー登録を実施しました。',
+            'status' => 'info'
+        ]);
     }
 
     /**
@@ -114,7 +117,10 @@ class OwnersController extends Controller
         $owner->password = Hash::make($request->password);
         $owner->save();
 
-        return redirect()->route('admin.owners.index')->with('message', 'オーナー情報を更新しました');
+        return redirect()->route('admin.owners.index')->with([
+            'message' => 'オーナー情報を更新しました',
+            'status' => 'info'
+        ]);
     }
 
     /**
@@ -125,6 +131,34 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete(); //ソフトデリート
+        return redirect()->route('admin.owners.index')->with(
+            [
+                'message' => 'オーナー情報を削除しました。',
+                'status' => 'alert'
+            ]
+        );
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function expiredOwnerIndex()
+    {
+        $expiredOwners = Owner::onlyTrashed()->get();
+        return view('admin.expired-owners', compact('expiredOwners'));
+    }
+
+    public function expiredOwnerDestroy($id)
+    {
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()->route('admin.expired-owners.index')->with(
+            [
+                'message' => 'オーナー情報を完全に削除しました。',
+                'status' => 'alert'
+            ]
+        );
     }
 }
--- src/php/resources/views/admin/expired-owners.blade.php
+++ src/php/resources/views/admin/expired-owners.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            オーナー管理
+            期限切れオーナー管理
         </h2>
     </x-slot>
 
@@ -15,10 +15,6 @@
                         <div class="container px-5 mx-auto">
                             {{-- @if (session('message')) --}}
                             <x-flash-message status="session('status')" />
-                            <div class="flex justify-end mb-4"><button
-                                    onclick="location.href='{{ route('admin.owners.create') }}'"
-                                    class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
-                            </div>
 
                             <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                                 <table class="table-auto w-full text-left whitespace-no-wrap">
@@ -32,37 +28,29 @@
                                                 メールアドレス</th>
                                             <th
                                                 class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
-                                                作成日</th>
-                                            <th
-                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
-                                            </th>
+                                                期限が切れた日</th>
                                             <th
                                                 class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
                                             </th>
                                         </tr>
-                                        @foreach ($owners as $owner)
+                                        @foreach ($expiredOwners as $owner)
                                     </thead>
                                     <tbody>
                                         <tr>
                                             <td class="px-4 py-3"> {{ $owner->name }}
                                             </td>
                                             <td class="px-4 py-3">{{ $owner->email }}</td>
-                                            <td class="px-4 py-3"> {{ $owner->created_at->diffForHumans() }}
+                                            <td class="px-4 py-3"> {{ $owner->deleted_at->diffForHumans() }}
                                             </td>
-                                            <td class="px-4 py-3">
-                                                <button
-                                                    onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id]) }}'"
-                                                    class="text-white bg-indigo-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集</button>
-                                            </td>
+
                                             <form id="delete_{{ $owner->id }}"
-                                                action="{{ route('admin.owners.destroy', ['owner' => $owner->id]) }}"
+                                                action="{{ route('admin.expired-owners.destroy', ['owner' => $owner->id]) }}"
                                                 method="POST">
                                                 @csrf
-                                                @method('delete')
                                                 <td class="px-4 py-3">
                                                     <a href="#" data-id="{{ $owner->id }}"
                                                         onclick="deletePost(this)"
-                                                        class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded text-lg">削除</a>
+                                                        class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded text-lg">完全に削除</a>
 
                                                 </td>
                                             </form>
