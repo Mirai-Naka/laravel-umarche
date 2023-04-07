dec1b86db0f33e009f06f2b5afdb7eb028d369ba
mirai <nakajimami11@gmail.com>
2023-04-07 11:26:21 +0900

    store

--- src/php/app/Services/imageService.php
+++ src/php/app/Services/imageService.php
@@ -10,10 +10,16 @@ class ImageService
 
     public static function upload($imageFile, $folderName)
     {
+        if (is_array($imageFile)) {
+            $file = $imageFile['image'];
+        } else {
+            $file = $imageFile;
+        }
+
         $fileName = uniqid(rand() . '_'); //ランダムなファイル名作成
-        $extension = $imageFile->extension(); //拡張子をつける
+        $extension = $file->extension(); //拡張子をつける
         $fileNameToStore = $fileName . '.' . $extension;
-        $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+        $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode();
 
         Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage);
 
